05b6ab8a4c15984d970727c4b6556f4a906547cf
k_rahul <108k.rahul@gmail.com>
2025-11-06 23:39:42 +0530

    practice 1295

--- ARRAYS/practice.cpp
+++ ARRAYS/practice.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    cout << a<<endl;
+    int num = 1234;
+    cout << num<<endl;
+
+    int lastdigit = 0;
+    int sum = 0;
+
+    while(num > 0){
+        lastdigit = num%10;
+        sum+=lastdigit; 
+        num = num/10;
+        cout<<lastdigit<<" added added to the sum!"<<endl;
+    }
+
+    cout<<sum<<endl;
     return 0;
 }
