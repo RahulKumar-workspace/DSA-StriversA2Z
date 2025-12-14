2169c2fa577d6c6e1fda6a6a8a5ceb79cc850a0d
RahulKumar-workspace <rahulkumar.workspace@gmail.com>
2025-12-14 11:56:27 +0530

    Conver a binary no. to decimal

--- Bit_Manipulation/basic.cpp
+++ Bit_Manipulation/basic.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 string convert2binary(int n);
+int convert2decimal(string x);
 
 //Tc = Log(base2)n, coz dividing by 2 each time. (n%2)
 //Sc = Same as Tc coz, whatever no of steps i'm taking the no or remainder is same.
@@ -18,12 +19,28 @@ string convert2binary(int n){
     return res;
 }
 
+
+//Tc = O(leng of the string)
+//Sc = O(1), coz we are only stroying it in one int.
+int convert2decimal(string x){
+    int len = x.length();
+    int num = 0;
+    int power2=1;
+    for(int i=len-1; i>=0; i--){
+        if(x[i] == '1') num+=power2;
+        power2 = power2*2;
+    }
+    return num;
+}
+
 int main(){
     int n = 56;
     // cout<<"Enter no: ";
     // cin>>n;
 
-    cout<<n<<" = ";
-    cout<<convert2binary(n)<<endl;
+    cout<<n<<" = "<<convert2binary(n)<<endl;
+
+    string x = "111000";
+    cout<<x<<" = "<<convert2decimal(x)<<endl;
     return 0;
 }
