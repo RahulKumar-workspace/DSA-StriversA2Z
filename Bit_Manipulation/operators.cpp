5d55378221718e6150af174f7767d7e2d2960e01
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-02 10:12:45 +0530

    swap 2 no's using only 2 variables

---
+++ Bit_Manipulation/operators.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+// // SWAP 2 NO's using only 2 variables:-
+// int main(){
+//     int a = 5;
+//     int b = 10;
+//     // a = a^b;
+//     // b = a^b;
+//     // a = a^b;
+
+//     a^=b;
+//     b^=a;
+//     a^=b;
+
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+//     return 0;
+// }
+
+
+
+int main(){
+    
+    return 0;
+}
