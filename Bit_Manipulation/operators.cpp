3c0ae1c5a3144c606d3df35e17a8eaf93c693962
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-02 10:58:25 +0530

    check if i'th bit is Set or not using Shift operators

--- Bit_Manipulation/operators.cpp
+++ Bit_Manipulation/operators.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 
 // // SWAP 2 NO's using only 2 variables:-
+// // Soln? -> use XOR operator.
 // int main(){
 //     int a = 5;
 //     int b = 10;
@@ -21,7 +22,22 @@ using namespace std;
 
 
 
+//Check if i'th bit is set(1) or not(Without converting a no into binary.).
+//Soln? -> use Shift operators
 int main(){
-    
+    int N = 13;
+    int i = 2;
+
+    // if((N&(1<<i))!=0){
+    //     cout<<"Set"<<endl;
+    // }else{
+    //     cout <<"Not Set"<<endl;
+    // }
+
+    if(((N>>i)&1) == 0){
+        cout<<"Not Set"<<endl;
+    }else{
+        cout<<"Set"<<endl;
+    }
     return 0;
 }
