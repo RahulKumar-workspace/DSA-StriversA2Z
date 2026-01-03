602c8c00a9fbad2657ec85dfd9d4f489329c2c18
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-03 11:12:59 +0530

    Clear the i'th bit

--- Bit_Manipulation/operators.cpp
+++ Bit_Manipulation/operators.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-// // Q1: SWAP 2 NO's using only 2 variables:-
-// // Soln? -> use XOR operator.
+/* Q1: SWAP 2 NO's using only 2 variables:-
+Soln? -> use XOR operator. */
 // int main(){
 //     int a = 5;
 //     int b = 10;
@@ -21,8 +21,8 @@ using namespace std;
 
 
 
-// // Q2: Check if i'th bit is set(1) or not(Without converting a no into binary.).
-// //Soln? -> use Shift operators
+/* Q2: Check if i'th bit is set(1) or not(Without converting a no into binary.).
+Soln? -> use Shift operators. */
 // int main(){
 //     int N = 13;
 //     int i = 2;
@@ -42,14 +42,34 @@ using namespace std;
 // }
 
 
-// Q3: Set the i'th bit.
-// Way -> put '1' underneath the ith bit then do OR operation.
-int main(){
-    int N = 9;
-    int i = 2;
+/* Q3: Set the i'th bit.
+Way -> put '1' underneath the ith bit then do OR operation. */
+// int main(){
+//     int N = 9;
+//     int i = 2;
 
-    N = (N | (1 << i));
-    cout<<N<<endl;
+//     N = (N | (1 << i));
+//     cout<<N<<endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
+/* Q4: Clear the i'th bit.
+Way -> Put '1' underneath the i'th bit. 
+Do NOT operation on that part to make the bit underneath the i'th bit as 0 rest all as 1's. 
+Now do & operation. */
+// int main(){
+//     int N = 13;
+//     int i = 1;
+
+//     N = (N & ~(1<<i));
+
+//     cout<<N<<endl;
+//     return 0;
+// }
+
+
+// int main(){
+
+//     return 0;
+// }
