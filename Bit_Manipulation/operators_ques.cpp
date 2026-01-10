0c12d818c91d18f62b55756adb0766c3153f6b1e
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-10 17:38:17 +0530

    alternative way to count the no of bits

--- Bit_Manipulation/operators_ques.cpp
+++ Bit_Manipulation/operators_ques.cpp
@@ -140,9 +140,14 @@ now yo'll nocice that only the bits which needs to flip are turning into '1'
 so not count the no of bits from the result = count*/
 int main(){
     int start = 10;
-    int end = 11;
+    int end = 7;
     int ans = start ^ end;
     int count = 0;
+    // while(ans != 0){
+    //     ans = ans & (ans-1);
+    //     count++;
+    // }
+//or
     for(int i=0; i<=31; i++){
         if(ans & (1<<i)){
             count++;
