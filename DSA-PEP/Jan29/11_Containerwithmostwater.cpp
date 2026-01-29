91179bbfbe1257a60871ba6d3a6a98a959386a0e
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-29 16:43:09 +0530

    Leetcode 11 - Container with the most water Optimal approach

--- DSA-PEP/Jan29/11_Containerwithmostwater.cpp
+++ DSA-PEP/Jan29/11_Containerwithmostwater.cpp
@@ -18,10 +18,32 @@ int maxAreaBrute(vector<int>& arr){
     return maxArea;
 }
 
+//Optimal: Time: O(n), Space: O(1)
+int maxAreaOptimal(vector<int>& arr){
+    int maxArea = 0;
+    int n = arr.size();
+    int left = 0;
+    int right = n-1;
 
+    while(left<right){
+        int width = right - left;
+        int height = min(arr[left],arr[right]);
+
+        maxArea = max(maxArea,width*height);
+
+        if(arr[left] < arr[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+
+    return maxArea;
+}
 
 int main(){
     vector<int> arr = {1,8,6,2,5,4,8,3,7};
     cout<<"maxArea: "<<maxAreaBrute(arr)<<endl;
+    cout<<"maxArea: "<<maxAreaOptimal(arr)<<endl;
     return 0;
 }
