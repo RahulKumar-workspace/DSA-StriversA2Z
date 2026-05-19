ff570b1d87422d6957fa74838ba50ddbbf5e2f30
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-05-19 21:31:40 +0530

    Sum 2 2D arrays

--- Self/2D-Array/Intro.cpp
+++ Self/2D-Array/Intro.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void print2Darray(int arr[3][4], int row, int col){
+void print2Darray(int arr[][4], int row, int col){
     for(int i=0; i<row; i++)
     for(int j=0; j<col; j++)
     cout<<arr[i][j]<<" ";
@@ -10,10 +10,26 @@ void print2Darray(int arr[3][4], int row, int col){
     cout<<endl;
 }
 
-int main(){
-    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+void sumof2Darray(int arr1[][4], int arr2[][4], int row, int col){
+    int ans[3][4];
+    for(int i=0; i<row; i++)
+    for(int j=0; j<col; j++)
+    ans[i][j] = arr1[i][j] + arr2[i][j];
 
-    print2Darray(arr, 3,4);
+    for(int i=0; i<row; i++)
+    for(int j=0; j<col; j++)
+    cout<<ans[i][j]<<" ";
+
+    cout<<endl;
+    
+}
+
+int main(){
+    int arr1[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int arr2[3][4] = {1,3,5,7,9,10,11,12,13,15,17,20};
+    print2Darray(arr1, 3,4);
+
+    sumof2Darray(arr1, arr2, 3, 4);
 
     return 0;
 }
