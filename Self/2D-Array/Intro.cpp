87b6e1bc4c6bc04355a3dc7790720b84e984694b
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-05-19 21:44:12 +0530

    Sync

--- Self/2D-Array/Intro.cpp
+++ Self/2D-Array/Intro.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+// We HAVVE to give the 'column' size!!
 void print2Darray(int arr[][4], int row, int col){
     for(int i=0; i<row; i++)
     for(int j=0; j<col; j++)
@@ -10,6 +11,8 @@ void print2Darray(int arr[][4], int row, int col){
     cout<<endl;
 }
 
+
+// For this simple method their size HAS to be the same otherwise it won't work.
 void sumof2Darray(int arr1[][4], int arr2[][4], int row, int col){
     int ans[3][4];
     for(int i=0; i<row; i++)
@@ -29,6 +32,7 @@ int main(){
     int arr2[3][4] = {1,3,5,7,9,10,11,12,13,15,17,20};
     print2Darray(arr1, 3,4);
 
+// Print the sum of 2 2D array
     sumof2Darray(arr1, arr2, 3, 4);
 
     return 0;
